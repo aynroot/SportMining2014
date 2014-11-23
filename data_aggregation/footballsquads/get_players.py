5938eed6c3c68780bfcab643c042fc07d8599bc9
Klimmy <spbmilk@mail.ru>
2014-11-23 21:27:43 +0300

    Rewrite scripts for Python 2.7

--- data_aggregation/footballsquads/get_players.py
+++ data_aggregation/footballsquads/get_players.py
@@ -1,21 +1,21 @@
-﻿import urllib.request
-import urllib.error
+﻿# Python 2.7
+import urllib
 import re
 from bs4 import BeautifulSoup
 
 f = open("Players.csv", "ab")
 reg = re.compile('\S[a-z A-Z]*')
 for y_1 in range(2000,2014):
-    print("Getting data for season " + str(y_1) + "/" + str(y_1+1))
+    print "Getting data for season " + str(y_1) + "/" + str(y_1+1)
     url_1 = "http://www.footballsquads.co.uk/eng/" + str(y_1) + "-" + str(y_1+1) + "/faprem.htm"
-    page_1 = urllib.request.urlopen(url_1)
+    page_1 = urllib.urlopen(url_1)
     soup_1 = BeautifulSoup(page_1)
     teams = soup_1.findAll('h5')
     for m in range(0,len(teams)):
         f = open("Players.csv", "ab")
         url_2 = "http://www.footballsquads.co.uk/eng/2000-2001/" + str(teams[m].a.get('href'))
-        print("Getting data for team " + str(m+1))
-        page_2 = urllib.request.urlopen(url_2)
+        print "Getting data for team " + str(m+1)
+        page_2 = urllib.urlopen(url_2)
         soup_2 = BeautifulSoup(page_2)
         players = soup_2.findAll('td')
         for n in range(9,len(players),8):
@@ -25,4 +25,3 @@ for y_1 in range(2000,2014):
                 except ValueError:
                     f.write(b"Season" + str(y_1).encode('utf8') + b"/" + str(y_1+1).encode('utf8') + b"\t" + ' '.join(reg.findall(players[n].string)).encode('utf8') + b"\t" + ' '.join(reg.findall(players[n+1].string)).encode('utf8') + b"\t" + str(players[n+4].string).encode('utf8')+ b"\n")
 f.close()
-
--- data_aggregation/soccerway/get_teams.py
+++ data_aggregation/soccerway/get_teams.py
@@ -1,48 +1,49 @@
-import urllib.request
-import urllib.error
+
+# Python 2.7
+import urllib
 import re
 from bs4 import BeautifulSoup
 
-# Создаем шаблон регулярного выражения, который понадобится в конце.
+# Creating regular expression, that will be useful in the end
 reg = re.compile('\S[a-z A-Z]*')
 
-# Создаем/открываем файл куда будем записывать инфу
+# Creating/opening file for saving data
 f = open("Teams.csv", "a")
 
-# Цикл для страничек с разных сезонов
-for y_1 in range(2000,2015):
+# Cycle for pages from different seasons
+for y_1 in range(2000,2001):
     f = open("Teams.csv", "a")
-    print("Getting data for " + str(y_1) + "/" + str(y_1+1))
-    # Т.к. ссылка отличается в зависимости от сезона делаем условие
+    print "Getting data for " + str(y_1) + "/" + str(y_1+1)
+    # Creating different types of links
     if y_1 < 2012:
         url = "http://uk.soccerway.com/national/england/premier-league/" + str(y_1) + "-" + str(y_1+1) + "/"
     else:
-        url = "http://uk.soccerway.com/national/england/premier-league/" + str(y_1) + str(y_1+1) + "/"
-    # Обрабатываем два вида ошибок. Первый, сайт не прогрузился; Второй, сайт выдал пустую страничку
+    url = "http://uk.soccerway.com/national/england/premier-league/" + str(y_1) + str(y_1+1) + "/"
+    # Processing for two types of errors. 1 - site doesn't response; 2 - site gave empty page
     p = 0
     k = 0
     while p != 1:
         while k != 1:
-                try:
-                    page = urllib.request.urlopen(url)
-                except urllib.request.HTTPError:
-                    k += -1
-                    print("Mistake")
-                k +=1
+            try:
+                page = urllib.urlopen(url)
+            except urllib.HTTPError:
+                k -= 1
+                print "Mistake"
+            k += 1
         soup = BeautifulSoup(page)
         if soup.findAll('h2') == []:
-            p += -1
-        p += +1
+            p -= 1
+        p += 1
 
-    # Собираем инфу о командах сезона в две переменные (такая уж структура на сайте)
+    # Collecting info about teams
     teams_1 = soup.findAll(attrs={"class":"team team-a "})
     teams_2 = soup.findAll(attrs={"class":"team team-b "})
 
-    # Записываем в файл названия всех команд, используя шаблон регулярного выражения из начала
+    # Writing names of teams into the csv, using regular expresseion we've mentioned before
     for n in range(0,len(teams_1)):
         f.write("Season " + str(y_1) + "/" + str(y_1+1) + '\t' + reg.findall(teams_1[n].a.string)[0] + '\n')
     for n in range(0,len(teams_2)):
         f.write("Season " + str(y_1) + "/" + str(y_1+1) + '\t' +  reg.findall(teams_2[n].a.string)[0] + '\n')
 
-# Обязательно нужно закрыть файл, чтобы он сохранился
+# It's necessary to close the file, otherwise it won't be saved
 f.close()
